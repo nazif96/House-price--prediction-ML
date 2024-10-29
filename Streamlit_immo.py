f160ce1fbe3d37ee635f41601294a18fcbff41ca
Nazif Afolabi <afolabinazif96@gmail.com>
2024-10-29 21:27:55 +0100

    environnement

---
+++ Streamlit_immo.py
@@ -0,0 +1,7 @@
+import pandas as pd 
+import numpy as np
+import streamlit as st 
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+import plotly.express as px 
+from sklearn.model_selection import train_test_split 
